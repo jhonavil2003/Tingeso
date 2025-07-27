0b6656c9241646d28affe11d66a5afd2f695a02d
Jhon Avila <jf.avila@uniandes.edu.co>
2025-07-27 17:01:46 -0500

    Ajuste

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 app.use(express.static(__dirname + '/dist/mynewapp'));
-
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/mynewapp/index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, function() {
-    console.log("Server running on port 5000...");
-    });
+  res.sendFile(path.join(__dirname+
+    '/dist/mynewapp/index.html'));});
+app.listen(process.env.PORT || 8080);
